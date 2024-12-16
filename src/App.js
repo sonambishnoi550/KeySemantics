3cd491b9bf6f2ec50d319859300ba27ac1583382
sonambishnoi550 <bishnoisonam079@gmail.com>
2024-12-16 15:19:25 +0530

    add back to top

--- src/App.js
+++ src/App.js
@@ -7,20 +7,23 @@ import Faq from './components/Faq'
 import KeySementic from './components/KeySementic'
 import Footer from './common/Footer'
 import FormValidation from './common/FormValidation'
+import BackToTop from './common/BackToTop'
 
 const App = () => {
   return (
-    <div>
-      <Hero />
-      <KeySementicWork />
-      <KeySementic/>
-      <Faq />
-      <Search />
-      <Security />
-      <FormValidation/>
-      <Footer/>
-      
-    </div>
+    <>
+      <>
+        <Hero />
+        <KeySementicWork />
+        <KeySementic />
+        <Faq />
+        <Search />
+        <Security />
+        <FormValidation />
+        <Footer />
+      </>
+      <BackToTop />
+    </>
   )
 }
 
---
+++ src/common/BackToTop.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const BackToTop = () => {
+    const [isVisible, setIsVisible] = useState(false);
+    window.onscroll = () => {
+        if (window.scrollY > 300) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+
+    return (
+        <div>
+            {isVisible && (
+                <button
+                    onClick={scrollToTop}
+                    className="fixed bottom-5 right-5 bg-purple text-darkblue p-2 w-12 border border-darkblue rounded-lg flex justify-center"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="white"
+                        viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                            d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708z" />
+                        <path fill-rule="evenodd"
+                            d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708z" />
+                    </svg>
+                </button>
+            )}
+        </div>
+    );
+};
+
+export default BackToTop;
